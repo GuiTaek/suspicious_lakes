e1dcea47ab2c1c2142dc2faa4b2c05ba0fd44a1c
GuiTaekM <guitaekm@gmail.com>
2026-02-05 22:17:02 +0100

    fix broken config values
    they are probably again broken, see later commits

--- src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
+++ src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
@@ -3,11 +3,11 @@ package com.gmail.guitaekm.enderlakes;
 import java.util.List;
 
 public class ConfigValues {
-    public int nrLakes = 276_987_611; // correct: 84_890_419
+    public int nrLakes = 84_890_419;
     public double powerDistance = 1.7;
     public List<Integer> cycleWeights = List.of(
             0, 5, 5, 3, 3, 1, 1
     );
-    public int[] factsPhi = new int[]{2, 5, 27698761}; // correct: new int[]{2, 3, 17, 41, 53, 383};
+    public int[] factsPhi = new int[]{2, 3, 17, 41, 53, 383};
     public int minimumDistance = 2;
 }
