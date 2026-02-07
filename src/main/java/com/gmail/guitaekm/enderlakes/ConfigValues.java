0407ac38064ce3647a81794f73a9fd65abe1af6a
GuiTaekM <guitaekm@gmail.com>
2026-02-07 01:14:31 +0100

    fix config for real this time (tested)

--- src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
+++ src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
@@ -3,11 +3,12 @@ package com.gmail.guitaekm.enderlakes;
 import java.util.List;
 
 public class ConfigValues {
-    public int nrLakes = 84_890_419;
+    public int nrLakes = 42_445_223;
     public double powerDistance = 1.7;
     public List<Integer> cycleWeights = List.of(
             0, 5, 5, 3, 3, 1, 1
     );
-    public int[] factsPhi = new int[]{2, 3, 17, 41, 53, 383};
+    public int[] factsPhi = new int[] { 2, 21222611 };
+
     public int minimumDistance = 2;
 }
