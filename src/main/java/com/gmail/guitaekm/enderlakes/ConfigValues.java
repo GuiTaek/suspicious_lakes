8dfd9b5dffaa245876e980400d11d7183f252816
GuiTaekM <guitaekm@gmail.com>
2026-02-16 17:33:56 +0100

    fiddle with the parameters
    now I'm finally satisfied -- lakes are more common to the spawn, there is a probability
    that you can hop from when lake group to another and 0, 1000 doesn't have a lot more lakes
    than 1000, 1000

--- src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
+++ src/main/java/com/gmail/guitaekm/enderlakes/ConfigValues.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class ConfigValues {
     public static final int nrLakes = 42_445_223;
-    public static final double powerDistance = 1.7;
+    public static final double powerDistance = 1.1;
     public static final List<Integer> cycleWeights = List.of(
             0, 5, 5, 3, 3, 1, 1
     );
     public static final int[] factsPhi = new int[] { 2, 21222611 };
 
-    public static final double minimumDistance = 32;
+    public static final double minimumDistance = 12;
     public static final double alpha = 0.5;
 
     //better don't touch this value, it's so difficult to balance so the game doesn't crash immediately
-    public static final double lambda = 5e-3;
+    public static final double lambda = 0.01;
     public static final int lastUnsafeChunkCoord = 64;
 }
